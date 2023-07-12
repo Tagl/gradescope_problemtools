d31ffe673f2785ab3dac81957699b084435ddd49
Tagl <arnara14@ru.is>
2023-07-12 11:18:04 +0000

    Remove sudo

--- extra_setup.sh
+++ extra_setup.sh
@@ -2,9 +2,9 @@
 
 pushd /autograder/source
 apt-get install -y python3 python3-pip python3-dev g++
-sudo add-apt-repository ppa:pypy/ppa
-sudo apt update
-sudo apt install pypy3
+add-apt-repository ppa:pypy/ppa
+apt update
+apt install pypy3
 pip3 install -r requirements.txt
 g++ -O3 -o default_validator default_validator.cpp
 popd
