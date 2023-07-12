c99d80f08929b06c1488fccd0969fb86199771d3
Tagl <arnara14@ru.is>
2023-07-12 10:55:24 +0000

    Initialize submodules

--- extra_setup.sh
+++ extra_setup.sh
@@ -4,6 +4,7 @@ pushd /autograder/source
 apt-get install -y python3 python3-pip python3-dev g++
 pip3 install -r requirements.txt
 g++ -O3 -o default_validator default_validator.cpp
+git submodule update --init --recursive
 popd
 
 pushd /autograder/source/problems/*/data
