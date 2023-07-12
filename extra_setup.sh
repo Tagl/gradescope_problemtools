b10459c6c753026938937ee6a06c3253a5a036b5
Tagl <arnara14@ru.is>
2023-07-12 10:39:37 +0000

    Run data generator on setup

--- extra_setup.sh
+++ extra_setup.sh
@@ -5,3 +5,7 @@ apt-get install -y python3 python3-pip python3-dev g++
 pip3 install -r requirements.txt
 g++ -O3 -o default_validator default_validator.cpp
 popd
+
+pushd /autograder/source/problems/*/data
+./generator
+popd
