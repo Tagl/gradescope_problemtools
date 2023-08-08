f0839e2f28eb18d4b4915b0d4188bcd9f640d4ab
Tagl <arnara14@ru.is>
2023-08-08 17:39:38 +0000

    Allow empty problems directory for no autograding

--- extra_setup.sh
+++ extra_setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-pushd /autograder/source
+MAINDIR="/autograder/source"
+PROBLEMSDIR="/autograder/source/problems"
+
+pushd "$MAINDIR"
 apt-get install -y python3 python3-pip python3-dev g++
 add-apt-repository ppa:pypy/ppa
 apt update
@@ -9,7 +12,11 @@ pip3 install -r requirements.txt
 g++ -O3 -o default_validator default_validator.cpp
 popd
 
-pushd /autograder/source/problems/*/data
-dos2unix generator
-. ./generator
-popd
+if [ -d "$PROBLEMSDIR" ]; then
+    for problemdir in "$PROBLEMSDIR/*"; do
+        pushd "$problemdir/data"
+        dos2unix generator
+        . ./generator
+        popd
+    done
+fi
